539608d5479a390f269ee08d1036b6b1a35b5b19
Vishal Pattar <104265753+Vishal-Pattar@users.noreply.github.com>
2023-08-18 05:14:44 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,20 @@
+var cat = ["Games","Machine Learning", "TMKOC", "Current Affairs", "Music", "Gadgets", "Technology"]
+
+details = [
+    ["12.23","videos/sample1.mp4","Curvy road on a tree covered hill", "Goa Vlogs", "12K views", "2 week ago"],
+    ["4.06","videos/sample2.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "7 week ago"],
+    ["6.03","videos/sample3.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "5 week ago"],
+    ["2.10","videos/sample4.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "1 week ago"],
+    ["8.45","videos/sample5.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "8 week ago"],
+    ["7.15","videos/sample6.mp4","Earth", "Goa Vlogs", "12K views", "6 week ago"],
+    ["12.23","videos/sample7.mp4","Curvy road on a tree covered hill", "Goa Vlogs", "12K views", "2 week ago"],
+    ["4.06","videos/sample8.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "7 week ago"],
+    ["6.03","videos/sample9.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "5 week ago"],
+    ["2.10","videos/sample10.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "1 week ago"],
+    ["12.23","videos/sample11.mp4","Curvy road on a tree covered hill", "Goa Vlogs", "12K views", "2 week ago"],
+    ["4.06","videos/sample12.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "7 week ago"],
+    ["6.03","videos/sample13.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "5 week ago"],
+    ["2.10","videos/sample14.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "1 week ago"],
+    ["8.45","videos/sample15.mp4","Beautiful Beach", "Goa Vlogs", "12K views", "8 week ago"],
+    ["7.15","videos/sample16.mp4","Earth", "Goa Vlogs", "12K views", "6 week ago"]
+]
---
+++ script.js
@@ -0,0 +1,43 @@
+function readCSVFile() {
+    var files = document.querySelector('#file').files;
+    var file = files[0];
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function (event) {
+        var csvdata = event.target.result;
+        var rowData = csvdata.split('\n');
+        for (var row = 1; row < rowData.length; row++) {
+            console.log(row);
+        }
+    }
+}
+function scaleup(a){
+    const ele = a.getElementsByClassName("thumbnail-image")[0];
+    a.style.transform = "scale(1.15)";
+    a.style.zIndex = "10";
+    a.style.boxShadow = "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px";
+    a.style.borderRadius = "1rem";
+    const attr1 = document.createAttribute("autoplay","true");
+    const attr2 = document.createAttribute("muted","true");
+    const attr3 = document.createAttribute("controls","true");
+    const attr4 = document.createAttribute("loop","true");
+    ele.setAttributeNode(attr1);
+    ele.setAttributeNode(attr2);
+    ele.setAttributeNode(attr3);
+    ele.setAttributeNode(attr4);
+    console.log(ele);
+}
+function scaledown(a){
+    const ele = a.getElementsByClassName("thumbnail-image")[0];
+    a.style.transform = "scale(1.0)";
+    a.style.zIndex = "1";
+    a.style.boxShadow = "none";
+    a.style.borderRadius = "none";
+    if(ele.hasAttribute("controls")){
+        ele.removeAttribute("autoplay");
+        ele.removeAttribute("muted");
+        ele.removeAttribute("controls");
+        ele.removeAttribute("loop");
+    }
+}
+
